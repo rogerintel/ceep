374e749d2e866f137d2d86eef40ddcda5146318f
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-03-24 23:11:50 -0300

    Removendo e editando cartões

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,19 +1,38 @@
-const Mural = (function(_render, Filtro){
+const Mural = (function (_render, Filtro) {
     "use strict"
-    let cartoes = []
+    let cartoes = (JSON.parse(localStorage.getItem("cartoes")) || [])
+        .map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo));
+
+    cartoes.forEach(cartao => {
+        preparaCartao(cartao);
+    })
+
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render()
+
+    function preparaCartao(cartao) {
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", () => {
+            cartoes = cartoes.slice(0)
+            cartoes.splice(cartoes.indexOf(cartao), 1)
+            salvaCartoes()
+            render()
+        })
+    }
 
     Filtro.on("filtrado", render)
 
-    function adiciona(cartao){
-        if (logado){
+    function salvaCartoes() {
+        localStorage.setItem("cartoes", JSON.stringify(
+            cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+        ))
+    }
+
+    function adiciona(cartao) {
+        if (logado) {
             cartoes.push(cartao)
-            cartao.on("mudanca.**", render)
-            cartao.on("remocao", () => {
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao), 1)
-                render()
-            })
+            salvaCartoes()
+            preparaCartao(cartao)
             render()
             return true
         } else {

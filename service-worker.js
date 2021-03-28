4955cc7217fea86da41ff027bbb8159ea8dbf15c
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-03-28 01:45:33 -0300

    Pegando todas as respostas do Service Worker

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,9 @@ self.addEventListener("fetch", function (){
     let pedido = event.request;
     let promiseResposta = caches.open("ceep-imagens")
         .then(cache => { return cache.match(pedido)})
-        .then(resposta => {return resposta})
+        .then(respostaCache => {
+            let resposta = respostaCache ? respostaCache : fetch(pedido)
+            return resposta
+        })
     event.respondWith(promiseResposta)
 })
